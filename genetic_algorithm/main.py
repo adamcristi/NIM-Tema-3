be93e7ef1086bcd84c5789343b0ffe3eae7f5169
alexd <alex.denis1206@gmail.com>
2021-05-09 16:04:57 +0300

    random_choice mutation

--- genetic_algorithm/main.py
+++ genetic_algorithm/main.py
@@ -3,6 +3,7 @@ import time
 from evaluation.evaluation_functions.eval_1 import eval_1
 from fitness.fitness_1 import fitness_1
 from genetic_algorithm.experiments.experiment_config import ExperimentConfig
+from genetic_algorithm.operators.mutation.random_choice import random_choice
 from genetic_algorithm.operators.selection.wheel_of_fortune import wheel_of_fortune_selection
 from genetic_algorithm.operators.crossover.double_cut_crossover import double_cut_crossover
 from genetic_algorithm.operators.mutation.add_one_mutation import add_one_mutation
@@ -10,13 +11,13 @@ from genetic_algorithm.experiments.run_experiment import run_experiment
 
 if __name__ == '__main__':
     data_file_path = []
-    # data_file_path += ["../data_files/myciel3.col"]  # myciel3.col (11,20), 4, MYC
+    data_file_path += ["../data_files/myciel3.col"]  # myciel3.col (11,20), 4, MYC
     data_file_path += ["../data_files/myciel7.col"]  # myciel7.col (191,2360), 8, MYC
-    # data_file_path += ["../data_files/queen13_13.col"]  # queen13_13.col (169,6656), 13, SGB
-    # data_file_path += ["../data_files/mulsol.i.5.col"]  # mulsol.i.5.col (186,3973), 31, REG
-    # data_file_path += ["../data_files/le450_15b.col"]  # le450_15b.col (450,8169), 15, LEI
-    # data_file_path += ["../data_files/fpsol2.i.1.col"]  # fpsol2.i.1.col (496,11654), 65, REG
-    # data_file_path += ["../data_files/inithx.i.2.col"]  # inithx.i.2.col (645, 13979), 31, REG
+    data_file_path += ["../data_files/queen13_13.col"]  # queen13_13.col (169,6656), 13, SGB
+    data_file_path += ["../data_files/mulsol.i.5.col"]  # mulsol.i.5.col (186,3973), 31, REG
+    data_file_path += ["../data_files/le450_15b.col"]  # le450_15b.col (450,8169), 15, LEI
+    data_file_path += ["../data_files/fpsol2.i.1.col"]  # fpsol2.i.1.col (496,11654), 65, REG
+    data_file_path += ["../data_files/inithx.i.2.col"]  # inithx.i.2.col (645, 13979), 31, REG
 
     """
     myciel3.col(11, 20), 4, MYC
@@ -31,8 +32,8 @@ if __name__ == '__main__':
     for path in data_file_path:
         experiment_config = ExperimentConfig(log_path=f"./logging_results/log_file_{time.time()}.txt",
                                              number_of_runs=30,
-                                             iterations=1000,
-                                             pop_size=100,
+                                             iterations=500,
+                                             pop_size=50,
                                              evaluation_function=eval_1,
                                              evaluation_kwargs={},
                                              fitness_function=fitness_1,
@@ -41,7 +42,7 @@ if __name__ == '__main__':
                                              selection_kwargs={},
                                              crossover_function=double_cut_crossover,
                                              crossover_kwargs={"crossover_rate": 0.4},
-                                             mutation_function=add_one_mutation,
-                                             mutation_kwargs={"mutation_rate": 0.02},
+                                             mutation_function=random_choice,
+                                             mutation_kwargs={"mutation_rate": 0.01},
                                              )
         run_experiment(path, experiment_config)
---
+++ genetic_algorithm/operators/mutation/random_choice.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def random_choice(population, mutation_rate=0.3, mutation_choosing_rate=-1):
+    pop_size, chromosome_size = population.shape
+
+    if mutation_choosing_rate > 0:
+        # Select chromosomes for mutation (save their indecies)
+        indecies = np.nonzero(np.random.rand(pop_size) < mutation_choosing_rate)[0]
+    else:
+        # Apply mutation using all chromosomes in the process
+        indecies = np.arange(0, pop_size)
+
+    for index in indecies:
+        for gene_index in range(0, chromosome_size):
+            if np.random.random() < mutation_rate:
+                # Apply mutation
+                new_val = np.random.randint(0, chromosome_size + 1 - gene_index)
+                while new_val == population[index][gene_index]:
+                    new_val = np.random.randint(0, chromosome_size + 1 - gene_index)
+                population[index][gene_index] = new_val
+
+    return population
